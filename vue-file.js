342d75003070bbcc1a1c96b88622ec969f611230
Kevin Sylvestre <kevin@ksylvest.com>
2017-04-09 08:16:37 -0700

    adding in basic seeds

--- vue-file.js
+++ vue-file.js
@@ -4,12 +4,28 @@
   VueFile.install = function (Vue) {
     Vue.directive("file", {
       bind: function (el, binding, vnode) {
-        console.log('bind', el, binding, vnode);
+        el.addEventListener('change', function (event) {
+          var files = event.target.files;
+          for (var index = 0; index < files.length; index++) {
+            var file = files[index];
+            (function (file) {
+              if (typeof FileReader !== "undefined") {
+                var reader = new FileReader();
+                reader.onload = function (event) {
+                  binding.value(file, event.target.result);
+                };
+                reader.readAsDataURL(file);
+              } else {
+                binding.value(file);
+              }
+            })(file);
+          }
+        });
       },
     });
   };
 
-  if (typeof exports === "object") {
+  if (typeof exports !== "undefined") {
     module.exports = VueFile;
   } else {
     global.VueFile = VueFile;
